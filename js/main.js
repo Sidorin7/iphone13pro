d1f86b775cf241480f052a917e4c311a83448141
Ivan Sidorin <123305436+Sidorin7@users.noreply.github.com>
2023-03-18 19:52:02 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("menu-open");
+});
+
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+// Цикл по всем ссылкам
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault(); // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute("href")
+      ? anchor.getAttribute("href")
+      : "body";
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
